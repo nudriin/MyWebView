4cb32b18232ac5a3efb54ade5879d04a92b216e0
Nurdin <mrsunnysummer@gmail.com>
2024-11-17 04:37:49 +0700

    feat: add print job

--- app/src/main/java/com/nudriin/mywebview/MainActivity.kt
+++ app/src/main/java/com/nudriin/mywebview/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.nudriin.mywebview
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
+import android.print.PrintAttributes
+import android.print.PrintJob
+import android.print.PrintManager
+import android.webkit.JavascriptInterface
 import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +18,17 @@ import com.nudriin.mywebview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val printJobs = mutableListOf<PrintJob>()
+
+    inner class WebAppInterface(private val context: Context) {
+        @JavascriptInterface
+        fun printPage() {
+            runOnUiThread {
+                createWebPrintJob(binding.webView)
+            }
+        }
+    }
+
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,10 +44,57 @@ class MainActivity : AppCompatActivity() {
         }
 
         val webView = binding.webView
-        webView.settings.javaScriptEnabled = true
+        with(webView.settings) {
+            javaScriptEnabled = true
+            domStorageEnabled = true  // Aktifkan DOM storage jika diperlukan
+        }
+
+        // Tambahkan JavaScript interface
+        webView.addJavascriptInterface(WebAppInterface(this), "Android")
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                // Inject JavaScript untuk menangkap event print
+                injectPrintCatcher()
+            }
+        }
+
         webView.webChromeClient = WebChromeClient()
 
         webView.loadUrl("https://antrihelu.nudriin.space")
+    }
 
+    private fun injectPrintCatcher() {
+        // JavaScript untuk menangkap event window.print()
+        val javascript = """
+            (function() {
+                var originalPrint = window.print;
+                window.print = function() {
+                    Android.printPage();
+                };
+            })();
+        """.trimIndent()
+
+        binding.webView.evaluateJavascript(javascript, null)
+    }
+
+    private fun createWebPrintJob(webView: WebView) {
+        (getSystemService(Context.PRINT_SERVICE) as? PrintManager)?.let { printManager ->
+            val jobName = "${getString(R.string.app_name)} Document"
+            val printAdapter = webView.createPrintDocumentAdapter(jobName)
+
+            val printJob: PrintJob = printManager.print(
+                jobName,
+                printAdapter,
+                PrintAttributes.Builder()
+                    .setMediaSize(PrintAttributes.MediaSize.ISO_A7)
+                    .setColorMode(PrintAttributes.COLOR_MODE_COLOR)
+                    .setMinMargins(PrintAttributes.Margins.NO_MARGINS)
+                    .build()
+            )
+
+            printJobs += printJob
+        }
     }
 }
