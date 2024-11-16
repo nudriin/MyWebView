a56b328f2ae26705f2175c0152a9c608c5a785c9
Nurdin <mrsunnysummer@gmail.com>
2024-11-17 03:52:41 +0700

    chore: upgrade compileSdk and targetSdk from 34 to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.nudriin.mywebview"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.nudriin.mywebview"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
